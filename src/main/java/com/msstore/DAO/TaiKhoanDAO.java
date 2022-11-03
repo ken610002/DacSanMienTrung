c94d3758e790dff1d0c3f970f76380fefbbe405e
Trần Mậu Phi <phitmps16509@fpt.edu.vn>
2022-11-03 15:20:47 +0700

    update forgpt password

--- src/main/java/com/msstore/DAO/TaiKhoanDAO.java
+++ src/main/java/com/msstore/DAO/TaiKhoanDAO.java
@@ -1,9 +1,12 @@
 package com.msstore.DAO;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.msstore.Entity.TaiKhoan;
 
 public interface TaiKhoanDAO extends JpaRepository<TaiKhoan, String>{
 
+	Optional<TaiKhoan> findByEmail(String email);
 }
